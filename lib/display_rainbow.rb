d62c48a71e06a676bae2793d6cec92496f0e52fd
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-06-02 04:47:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-# Write your #display_rainbow method here
+
 def display_rainbow(arg)
   puts "R: #{arg[0]}, O: #{arg[1]}, Y: #{arg[2]}, G: #{arg[3]}, B: #{arg[4]},"
 end
